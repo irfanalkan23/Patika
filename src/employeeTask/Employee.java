a8e57ffe10507656e9a97fc0ec6837846f996303
irfan <23pointalpha@gmail.com>
2022-10-18 12:11:07 +0300

    Örnek konsol çıktısı açıklaması üzerine yapılan değişiklikler

--- src/employeeTask/Employee.java
+++ src/employeeTask/Employee.java
@@ -6,10 +6,10 @@ public class Employee {
 
     private String name;
     private double salary;
-    private double workHours;
+    private int workHours;
     private int hireYear;
 
-    public Employee(String name, double salary, double workHours, int hireYear) {
+    public Employee(String name, double salary, int workHours, int hireYear) {
         setName(name);
         setSalary(salary);
         setWorkHours(workHours);
@@ -30,7 +30,7 @@ public class Employee {
         this.salary = salary;
     }
 
-    public void setWorkHours(double workHours) {
+    public void setWorkHours(int workHours) {
         if (workHours < 0){
             throw new InputMismatchException("Work hours can not be negative: " + workHours);
         }
@@ -50,7 +50,7 @@ public class Employee {
      * Calculates the tax to salary
      * No tax if salary is less than 1000TL
      * 3% tax if salary is more than or equal to 1000TL
-     * @param salary
+     * @param salary Maaş
      * @return tax amount
      */
     private double tax(double salary){
@@ -66,15 +66,14 @@ public class Employee {
     /**
      * Calculates the bonus payment.
      * 30TL bonus per hour over 40 working hours per week.
-     * Calculates bonus for 4 weeks (one month)
-     * @param workHours
+     * @param workHours Çalışma saati
      * @return bonus amount
      */
-    private double bonus(double workHours){
-        if (workHours>40.0){
-            return (workHours-40.0) * 30.0 * 4.0;
+    private double bonus(int workHours){
+        if (workHours>40){
+            return (workHours-40) * 30;
         } else {
-            return 0.0;
+            return 0;
         }
     }
 
@@ -84,18 +83,17 @@ public class Employee {
      * 5% increase in salary if less than 10 years
      * 10% increase in salary if more than 9 and less than 20 years
      * 15% increase in salary if more than 19 years
-     * @param hireYear
+     * @param hireYear Başlangıç yılı
      * @return salary raise amount
      */
     private double raiseSalary(int hireYear){
         double raiseSalaryAmount;
-        double salaryWithTaxAndBonus = salary + tax(salary) + bonus(workHours);
         if ( (2021-hireYear) < 10 ) {
-            raiseSalaryAmount = salaryWithTaxAndBonus * 0.05;
+            raiseSalaryAmount = salary * 0.05;
         } else if ( (2021-hireYear) > 9 && (2021-hireYear) < 20 ){
-            raiseSalaryAmount = salaryWithTaxAndBonus *  0.1;
+            raiseSalaryAmount = salary *  0.1;
         } else {
-            raiseSalaryAmount = salaryWithTaxAndBonus * 0.15;
+            raiseSalaryAmount = salary * 0.15;
         }
         return raiseSalaryAmount;
     }
@@ -103,16 +101,14 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", salary=" + salary +
-                ", workHours=" + workHours +
-                ", hireYear=" + hireYear +
-                ", tax =" + tax(salary) +
-                ", bonus =" + bonus(workHours) +
-                ", raise in salary =" + raiseSalary(hireYear) +
-                ", salary with tax and bonus =" + ( salary + raiseSalary(hireYear) + bonus(workHours) + tax(salary) ) +
-                ", total salary=" + ( salary + raiseSalary(hireYear) + bonus(workHours) ) +
-                '}';
+        return  "Adı : " + name +
+                "\nMaaşı : " + salary +
+                "\nÇalışma Saati : " + workHours +
+                "\nBaşlangıç Yılı : " + hireYear +
+                "\nVergi : " + tax(salary) +
+                "\nBonus : " + bonus(workHours) +
+                "\nMaaş Artışı : " + raiseSalary(hireYear) +
+                "\nVergi ve Bonuslar ile birlikte maaş : " + ( salary + raiseSalary(hireYear) - bonus(workHours) - tax(salary) ) +
+                "\nToplam Maaş : " + ( salary + raiseSalary(hireYear) );
     }
 }
--- src/employeeTask/EmployeeTest.java
+++ src/employeeTask/EmployeeTest.java
@@ -2,7 +2,7 @@ package employeeTask;
 
 public class EmployeeTest {
     public static void main(String[] args) {
-        Employee employee1 = new Employee("Ali Can Berk",2000.0, 50.0,2011);
+        Employee employee1 = new Employee("kemal",2000.0, 45,1985);
         System.out.println(employee1);
     }
 }
