ab84e535e23fbecf204fd30c1081f88f200c2f77
irfan <23pointalpha@gmail.com>
2022-11-01 12:56:26 +0300

    added javadoc description of the task

--- src/pratik/NotOrtalamasiHesaplayanProgram.java
+++ src/pratik/NotOrtalamasiHesaplayanProgram.java
@@ -3,7 +3,20 @@ package pratik;
 import java.util.Scanner;
 
 public class NotOrtalamasiHesaplayanProgram {
+
     public static void main(String[] args) {
+        /**
+         * Not Ortalaması Hesaplayan Program
+         * Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
+         *
+         * Ödev
+         * Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.
+         *
+         * Not : If ve Else kullanılmayacak...
+         *
+         * @author irfan
+         */
+
         int mat, fizik, kimya, turkce, tarih, muzik;
 
         Scanner input = new Scanner(System.in);
