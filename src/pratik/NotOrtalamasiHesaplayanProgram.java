e665d82800a7cfa9aec5ea21bde0bcbc20578076
irfan <23pointalpha@gmail.com>
2022-11-01 14:10:06 +0300

    added link to course page

--- src/pratik/NotOrtalamasiHesaplayanProgram.java
+++ src/pratik/NotOrtalamasiHesaplayanProgram.java
@@ -3,19 +3,9 @@ package pratik;
 import java.util.Scanner;
 
 public class NotOrtalamasiHesaplayanProgram {
+    //Link to course page: https://app.patika.dev/courses/java101/pratik-not-ortalamasi
 
     public static void main(String[] args) {
-        /**
-         * Not Ortalaması Hesaplayan Program
-         * Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
-         *
-         * Ödev
-         * Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.
-         *
-         * Not : If ve Else kullanılmayacak...
-         *
-         * @author irfan
-         */
 
         int mat, fizik, kimya, turkce, tarih, muzik;
 
@@ -47,3 +37,14 @@ public class NotOrtalamasiHesaplayanProgram {
         System.out.println(sinifGeçmeDurumu);
     }
 }
+/**
+ * Not Ortalaması Hesaplayan Program
+ * Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
+ *
+ * Ödev
+ * Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.
+ *
+ * Not : If ve Else kullanılmayacak...
+ *
+ * @author irfan
+ */
