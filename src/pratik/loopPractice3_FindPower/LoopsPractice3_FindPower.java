383b9c88ce7e4147649da4845bd8a0dd75a3c1af
irfan <23pointalpha@gmail.com>
2022-11-07 15:38:16 +0300

    LoopsPractice3_FindPower.java Readme.md

---
+++ src/pratik/loopPractice3_FindPower/LoopsPractice3_FindPower.java
@@ -0,0 +1,40 @@
+package pratik.loopPractice3_FindPower;
+
+import java.util.Scanner;
+
+public class LoopsPractice3_FindPower {
+
+    public static void main(String[] args) {
+
+        //Değişkenleri tanımlıyoruz
+        int limitNumber, result=1;
+
+        //Kullanıcıdan limit değeri alıyoruz
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bu program gireceğiniz pozitif tamsayıya kadar olan 4'ün ve 5'in kuvvetlerini bulur.\n" +
+                "Limit sayıyı giriniz : ");
+        limitNumber = input.nextInt();
+
+        //Girilen değer 1'den küçük ise hata mesajı veriyoruz ve tekrar değer girmesini istiyoruz
+        while (limitNumber < 1){
+            System.out.print("Hatalı giriş yaptınız! : " + limitNumber +
+                        "\nLütfen pozitif bir tamsayı giriniz : ");
+            limitNumber = input.nextInt();
+        }
+
+        //Limit değere kadar olan 4'ün kuvvetlerini yazdırıyoruz
+        System.out.println("4'ün kuvvetleri :");
+        for (int i=1; result <= limitNumber; i++){
+            System.out.println(result);
+            result *= 4;
+        }
+
+        //Limit değere kadar olan 5'in kuvvetlerini yazdırıyoruz
+        result = 1;
+        System.out.println("5'in kuvvetleri :");
+        for (int i=1; result <= limitNumber; i++){
+            System.out.println(result);
+            result *= 5;
+        }
+    }
+}
---
+++ src/pratik/loopPractice3_FindPower/Readme.md
@@ -0,0 +1,14 @@
+# Task
+Girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program.
+
+## Task Link
+https://app.patika.dev/courses/java101/pratik-two-power
+
+## Task Description
+Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz.
+
+## Notes
+ebebek Java & QA & SAP Spartacus Practicum.
+
+## Author
+İrfan ALKAN
