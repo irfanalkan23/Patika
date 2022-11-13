68448402dd84fc4a4bd2555c17e9c13369dc0c69
irfan <23pointalpha@gmail.com>
2022-11-13 13:22:14 +0300

    Calculator2.java Readme.md

---
+++ src/odev/hesapMakinesi2/Calculator2.java
@@ -0,0 +1,196 @@
+package odev.hesapMakinesi2;
+
+import java.util.Scanner;
+
+public class Calculator2 {
+
+    static void plus() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 0, i = 1;
+        while (true) {
+            System.out.print(i++ + ". sayı :");
+            number = scan.nextInt();
+            if (number == 0) {
+                break;
+            }
+            result += number;
+        }
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void minus() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result -= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void times() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 1, i = 1;
+
+        while (true) {
+            System.out.print(i++ + ". sayı :");
+            number = scan.nextInt();
+
+            if (number == 1)
+                break;
+
+            if (number == 0) {
+                result = 0;
+                break;
+            }
+            result *= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void divided() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        double number, result = 0.0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextDouble();
+            if (i != 1 && number == 0) {
+                System.out.println("Böleni 0 giremezsiniz.");
+                continue;
+            }
+            if (i == 1) {
+                result = number;
+                continue;
+            }
+            result /= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void factorial() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz :");
+        int n = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    //Mod Alma Fonksiyonu
+    static void mod(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bölünen sayıyı giriniz : ");
+        int n1 = scan.nextInt();
+        System.out.print("Bölen sayıyı giriniz : ");
+        int n2 = scan.nextInt();
+
+        //Mod alma işlemi
+        int result = n1 % n2;
+
+        System.out.println(n1 + " mod " + n2 + " = " + result);
+    }
+
+    //Dikdörtgen Alan ve Çevre Hesabı
+    static void rectangle(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Dikdörtgenin birinci kenar uzunluğunu giriniz : ");
+        int a = scan.nextInt();
+        System.out.print("Dikdörtgenin ikinci kenar uzunluğunu giriniz : ");
+        int b = scan.nextInt();
+
+        //Dikdörtgenin çevresi
+        int perimeter = 2 * (a + b);
+
+        //Dikdörtgenin alanı
+        int area = a * b;
+
+        System.out.println("Dikdörtgenin Alanı : " + perimeter + "\nDikdörtgenin Çevresi : " + area);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int select;
+        String menu = "1- Toplama İşlemi\n"
+                + "2- Çıkarma İşlemi\n"
+                + "3- Çarpma İşlemi\n"
+                + "4- Bölme işlemi\n"
+                + "5- Üslü Sayı Hesaplama\n"
+                + "6- Faktoriyel Hesaplama\n"
+                + "7- Mod Alma\n"
+                + "8- Dikdörtgen Alan ve Çevre Hesabı\n"
+                + "0- Çıkış Yap";
+
+        do {
+            System.out.println(menu);
+            System.out.print("Lütfen bir işlem seçiniz :");
+            select = scan.nextInt();
+            switch (select) {
+                case 1:
+                    plus();
+                    break;
+                case 2:
+                    minus();
+                    break;
+                case 3:
+                    times();
+                    break;
+                case 4:
+                    divided();
+                    break;
+                case 5:
+                    power();
+                    break;
+                case 6:
+                    factorial();
+                    break;
+                //Mod Alma
+                case 7:
+                    mod();
+                    break;
+                //Dikdörtgen Alan ve Çevre Hesabı
+                case 8:
+                    rectangle();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Yanlış bir değer girdiniz, tekrar deneyiniz.");
+            }
+        } while (select != 0);
+
+
+    }
+}
---
+++ src/odev/hesapMakinesi2/Readme.md
@@ -0,0 +1,19 @@
+# Task
+Hesap makinesi programı.
+
+## Task Link
+https://app.patika.dev/courses/java101/pratik-hesap-mak-2
+
+## Task Description
+Gelişmiş Hesap Makinesi
+Java ile kullanıcın seçtiği işlemleri yapan hesap makinesi yapıyoruz.
+
+Hesap makinesinin fonksiyonları :
+![img.png](img.png)
+Aynı projeye mod almak ve dikdörtgen alan çevre hesabını yapan metotları yazıp menüde işlevsel hale getiriniz.
+
+## Notes
+ebebek Java & QA & SAP Spartacus Practicum.
+
+## Author
+İrfan ALKAN
